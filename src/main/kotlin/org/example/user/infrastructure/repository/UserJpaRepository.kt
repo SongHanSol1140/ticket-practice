48df32240f60f415ba15420a16756380f5ae9fd8
songhansol <songhansol1140@gmail.com>
2026-03-09 18:24:42 +0900

    오타수정 getter 대신 private set 사용 티켓타입체크 model에서 ApiClientResolver로 이동 패키지 경로 수정

---
+++ src/main/kotlin/org/example/user/infrastructure/repository/UserJpaRepository.kt
@@ -0,0 +1,16 @@
+package org.example.user.infrastructure.repository
+
+import org.example.user.domain.enum.UserRole
+import org.example.user.domain.model.User
+import java.math.BigDecimal
+
+object UserJpaRepository {
+    private val users = listOf(
+        User(id = 1, name = "판매자1", role = UserRole.SELLER, money = BigDecimal.ZERO),
+        User(id = 2, name = "판매자2", role = UserRole.SELLER, money = BigDecimal.ZERO),
+        User(id = 3, name = "구매자1", role = UserRole.BUYER, money = BigDecimal(10000)),
+        User(id = 4, name = "구매자2", role = UserRole.BUYER, money = BigDecimal(50000)),
+    )
+
+    fun findByName(name: String): User? = users.find { it.name == name }
+}
